b4a64f908569b78636dbaa86b097e3ba7cdd850f
PandaDrunkard <tosho.hirasawa@gmail.com>
2018-02-11 12:12:52 +0900

    ソフトマックス関数にオーバーフロー対策を追加した

--- ch3/softmax_func.py
+++ ch3/softmax_func.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 def softmax(X):
-    exp_a = np.exp(X)
+    c = np.max(X)
+    exp_a = np.exp(X - c) # avoid overflow
     sum_exp_a = np.sum(exp_a)
     Y = exp_a / sum_exp_a
 
