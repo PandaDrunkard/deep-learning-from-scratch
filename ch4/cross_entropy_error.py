1acb5a9cd4b20128ec63de9acc50f1e36b4ddbcd
PandaDrunkard <tosho.hirasawa@gmail.com>
2018-03-15 22:26:17 +0900

    ミニバッチ対応

--- ch4/cross_entropy_error.py
+++ ch4/cross_entropy_error.py
@@ -1,8 +1,13 @@
 import numpy as np
 
 def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        y = y.reshape(1, y.size)
+        t = t.reshape(1, t.size)
+
+    batch_size = y.shape[0]
     delta = 1e-7
-    return -1 * np.sum(t * np.log(y + delta))
+    return -1 * np.sum(t * np.log(y + delta)) / batch_size
 
 def test_cross_entropy_error():
     t = np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
@@ -15,4 +20,4 @@ def test_cross_entropy_error():
     d = cross_entropy_error(y, t)
     print(d)
 
-# test_cross_entropy_error()
+test_cross_entropy_error()
