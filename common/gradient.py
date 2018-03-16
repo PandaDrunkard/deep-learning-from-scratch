fd0e423b57fc283e9f8d5dd89a18b3826540f027
PandaDrunkard <tosho.hirasawa@gmail.com>
2018-03-16 15:25:41 +0900

    update numerical_gradient for nD

--- common/gradient.py
+++ common/gradient.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def numerical_gradient(f, x):
+def numerical_gradient_1d(f, x):
     h = 1e-4
     # xと同じ形だが、要素が0.であるarrayを返す
     grad = np.zeros_like(x)
@@ -18,3 +18,27 @@ def numerical_gradient(f, x):
         x[idx] = act_x
     
     return grad
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    g = np.zeros_like(x)
+
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        org_x = x[idx]
+        
+        # f(x+h)
+        x[idx] = float(org_x) + h
+        fxh1 = f(x)
+
+        # f(x-h)
+        x[idx] = float(org_x) - h
+        fxh2 = f(x)
+
+        g[idx] = (fxh1 - fxh2) / (2*h)
+
+        x[idx] = org_x
+        it.iternext()
+    
+    return g
