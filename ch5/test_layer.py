34dc8df017319d765a64260e2a6674719acfdc94
PandaDrunkard <tosho.hirasawa@gmail.com>
2018-03-21 18:16:58 +0900

    sigmoid layer

--- ch5/test_layer.py
+++ ch5/test_layer.py
@@ -4,5 +4,18 @@ from common.layer import *
 import numpy as np
 
 x = np.array([[1.0, -0.5], [-2.0, 3.0]])
+
+relu_layer = Relu()
+relu_out = relu_layer.forward(x)
+relu_dx = relu_layer.backward(relu_out)
+
 print(x)
-print(x[x <= 0])
+print(relu_out)
+print(relu_dx)
+
+sigmoid_layer = Sigmoid()
+s_out = sigmoid_layer.forward(x)
+s_dx = sigmoid_layer.backward(s_out)
+
+print(s_out)
+print(s_dx)
